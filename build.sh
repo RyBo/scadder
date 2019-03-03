cfc75fdb6990262b95e09505542fed55d2237620
Ryan Bohannon <ryan.m.bohannon@gmail.com>
2019-03-03 13:35:58 -0700

    Have build.sh generate bitstream

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/opt/Xilinx/Vivado/2015.4/settings64.sh
-/opt/Xilinx/Vivado/2015.4/bin/vivado -mode batch -source vivado/build.tcl
---
+++ util/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#/opt/Xilinx/Vivado/2015.4/settings64.sh
+#/opt/Xilinx/Vivado/2015.4/bin/vivado -mode batch -source vivado/build.tcl
+
+/opt/Xilinx/Vivado/2015.4/bin/vivado -mode batch -source vivado/implement.tcl
