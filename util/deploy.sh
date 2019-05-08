fc3d36a8712908d901864f71725aa6b6de1244fb
Ryan Bohannon <ryan.m.bohannon@gmail.com>
2019-05-08 12:43:46 -0600

    Add Petalinux-tools deployment script

--- util/deploy.sh
+++ util/deploy.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-project-name = "microblaze-ocm"
-petalinux-project-name = "zynq-poc-app"
-SD-path = "/tmp"
+if [ $# -ne 2 ] 
+then
+    echo "Error: Missing Arguments"
+    echo "Provide a name for the project"
+    echo "Provide a path for the SD card"
+    echo "ex: ./build.sh microblaze-ocm /media/sdcard"
+    exit 1
+fi
+
+project_name=$1
+petalinux_project_name = "petalinix-$project_name"
+sd_path = $2
 
 # create the petalinux project
-petalinux-create --type project --template zynq --name $petalinux-project-name
+petalinux-create --type project --template zynq --name $petalinux_project_name
 cd $petalinux-project-name
+
 # supply the directory with the hardware files and use the default project settings
-echo "ESC" | petalinux-config --get-hw-description=/home/abdullah/project_9/project_9.sdk/
+echo "ESC" | petalinux-config --get-hw-description=build/$project_name.sdk/
+
 # build the petalinux application
 petalinux-build
+
 # create the application boot files
-petalinux-package --boot --fsbl sdk/FSBL/Debug/FSBL.elf --fpga build/$project-name.sdk/hw_platform/design_1_wrapper.bit --u-boot
+petalinux-package --boot --fsbl sdk/FSBL/Debug/FSBL.elf --fpga build/$project_name.sdk/hw_platform/design_1_wrapper.bit --u-boot
+
 # copy over to the SD Card
-cp image/linux/BOOT.bin $SD-path
-cp image/linux/image.ub $SD-path
+cp image/linux/BOOT.bin $sd_path
+cp image/linux/image.ub $sd_path
 
 echo "Petalinux project has been shipped to SD Card. Set the Zynq BOOT-MODE to 00110"
